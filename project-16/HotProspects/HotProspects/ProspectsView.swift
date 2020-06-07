c8c319b0ca7b6fd1b6ab2fdffc5a01eef682d89d
fer96 <fer96.dlrs@gmail.com>
2020-06-07 14:16:36 -0500

    [084] Project 16 - part six

--- project-16/HotProspects/HotProspects/ProspectsView.swift
+++ project-16/HotProspects/HotProspects/ProspectsView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 import CodeScanner
 
 enum FilterType {
@@ -57,9 +58,43 @@ extension ProspectsView {
 			person.name = details[0]
 			person.emailAddress = details[1]
 			
-			self.prospects.people.append(person)
+			self.prospects.add(person)
 		case .failure( _):
 			print("Scanning failed")
+		}
+	}
+	
+	func addNotification(for prospect: Prospect) {
+		let center = UNUserNotificationCenter.current()
+		
+		let addRequest = {
+			let content = UNMutableNotificationContent()
+			content.title = "Contact \(prospect.name)"
+			content.subtitle = prospect.emailAddress
+			content.sound = UNNotificationSound.default
+			
+			// This code was comment by testing purpose
+			//			var dateComponents = DateComponents()
+			//			dateComponents.hour = 9
+			//			let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+			let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+			
+			let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+			center.add(request)
+		}
+		
+		center.getNotificationSettings { settings in
+			if settings.authorizationStatus == .authorized {
+				addRequest()
+			} else {
+				center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+					if success {
+						addRequest()
+					} else {
+						print("D'oh")
+					}
+				}
+			}
 		}
 	}
 }
@@ -75,24 +110,27 @@ extension ProspectsView {
 							.font(.headline)
 						Text(prospect.emailAddress)
 							.foregroundColor(.secondary)
-							.contextMenu {
-								Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
-									self.prospects.toggle(prospect)
-								}
+					}
+					.contextMenu {
+						Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+							self.prospects.toggle(prospect)
+						}
+						
+						if !prospect.isContacted {
+							Button("Remind Me") {
+								self.addNotification(for: prospect)
+							}
 						}
 					}
 				}
 			}
-				
 			.navigationBarTitle(title)
-				
 			.navigationBarItems(trailing: Button(action: {
 				self.isShowingScanner = true
 			}) {
 				Image(systemName: "qrcode.viewfinder")
 				Text("Scan")
 			})
-				
 				.sheet(isPresented: $isShowingScanner) {
 					CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: self.handleScan)
 			}
