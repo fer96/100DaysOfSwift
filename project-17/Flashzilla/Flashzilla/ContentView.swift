e6b9417252bed30dac7b57d54ab22328e99fd5e0
fer96 <fer96.dlrs@gmail.com>
2020-06-10 12:48:57 -0500

    [087] Project 17 - part two
    	- Triggering events repeatedly using a timer
    	- How to be notified when your SwiftUI app moves to the background
    	- Supporting specific accessibility needs with SwiftUI

--- project-17/Flashzilla/Flashzilla/ContentView.swift
+++ project-17/Flashzilla/Flashzilla/ContentView.swift
@@ -9,41 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
-	// how far the circle has been dragged
-	@State private var offset = CGSize.zero
 	
-	// whether it is currently being dragged or not
-	@State private var isDragging = false
-	
+}
+
+// MARK: - View
+extension ContentView {
 	var body: some View {
-		// a drag gesture that updates offset and isDragging as it moves around
-		let dragGesture = DragGesture()
-			.onChanged { value in self.offset = value.translation }
-			.onEnded { _ in
-				withAnimation {
-					self.offset = .zero
-					self.isDragging = false
-				}
+		VStack {
+			Text("Hello, World!")
+				.onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+					print("Moving to the background!")
+			}
+			
+			Text("Hello, World!")
+				.onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+					print("Moving back to the foreground!")
+			}
+			
+			Text("Hello, World!")
+				.onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { _ in
+					print("User took a screenshot!")
+			}
 		}
-		
-		// a long press gesture that enables isDragging
-		let pressGesture = LongPressGesture()
-			.onEnded { value in
-				withAnimation {
-					self.isDragging = true
-				}
-		}
-		
-		// a combined gesture that forces the user to long press then drag
-		let combined = pressGesture.sequenced(before: dragGesture)
-		
-		// a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-		return Circle()
-			.fill(Color.red)
-			.frame(width: 64, height: 64)
-			.scaleEffect(isDragging ? 1.5 : 1)
-			.offset(offset)
-			.gesture(combined)
 	}
 }
 
--- project-17/notes.md
+++ project-17/notes.md
@@ -9,7 +9,15 @@
 * If you want to change that you can use the __highPriorityGesture()__ modifier to force the parent’s gesture to trigger instead
 * __simultaneousGesture()__ modifier to tell SwiftUI you want both the parent and child gestures to trigger at the same time
 
-
-## Making vibrations with UINotificationFeedbackGenerator and Core Haptics
-
 ## Disabling user interactivity with allowsHitTesting()
+
+* SwiftUI lets us control user interactivity in two useful ways, the first of which is the allowsHitTesting() modifier. When this is attached to a view with its parameter set to false, the view isn’t even considered tappable
+
+## How to be notified when your SwiftUI app moves to the background
+
+SwiftUI can detect when your app moves to the background (i.e., when the user returns to the home screen), when it comes back to the foreground, when the user takes a screenshot, and much more. This is all powered by Notification Center, which is the name for Apple’s internal message system – API that lets the system notify us when events happen, but also lets us post messages between different parts of our code.
+
+* UIApplication.significantTimeChangeNotification is called when the user changes their clock or when daylight savings time changes.
+* UIResponder.keyboardDidShowNotification is called when the keyboard is shown.
+
+Each of these notifications works in exactly the same way: use onReceive() to catch notifications from the publisher, then take whatever action you want.
