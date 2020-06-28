d9dc2a2697c8f97da422ea6fd14f7d9322cd2438
fer96 <fer96.dlrs@gmail.com>
2020-06-28 13:26:43 -0500

    [020] Project 2 - part two
    	- Guess which flag: random numbers
    	- From outlets to actions: creating an IBAction

--- project-02/GuessTheFlag-UIKit/GuessTheFlag-UIKit/ViewController.swift
+++ project-02/GuessTheFlag-UIKit/GuessTheFlag-UIKit/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
 	// MARK: - Properties
 	var countries = [String]()
 	var score = 0
+	var correctAnswer = 0
 	
 	// MARK: - Life cycle
 	override func viewDidLoad() {
@@ -55,10 +56,31 @@ class ViewController: UIViewController {
 		countries.append("us")
 	}
 	
-	private func askQuestion() {
+	private func askQuestion(action: UIAlertAction! = nil) {
+		countries.shuffle()
+		correctAnswer = Int.random(in: 0...2)
+		title = countries[correctAnswer].uppercased()
+		
 		button1.setImage(UIImage(named: countries[0]), for: .normal)
 		button2.setImage(UIImage(named: countries[1]), for: .normal)
 		button3.setImage(UIImage(named: countries[2]), for: .normal)
+	}
+	
+	// MARK: IBActions
+	@IBAction func buttonTapped(_ sender: UIButton) {
+		var title: String
+		
+		if sender.tag == correctAnswer {
+			title = "Correct"
+			score += 1
+		} else {
+			title = "Wrong"
+			score -= 1
+		}
+		
+		let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+		present(ac, animated: true)
 	}
 }
 
